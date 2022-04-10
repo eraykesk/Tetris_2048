d2eb239db6f539a1442d3ddceddb22cc42d00805
betulselvi <85995880+betulselvi@users.noreply.github.com>
2022-04-10 16:00:31 +0300

    Update game_grid.py
    Updated clear full lines for computing score

--- game_grid.py
+++ game_grid.py
@@ -121,11 +121,13 @@ class GameGrid:
         return self.game_over
 
     # Method for clearing full horizontal lines on the game board
+    # and computing score
     def clear_full_lines(self):
         # creating a new empty array to append the game board
         # after deleting the full line
         new_row = np.full((1, self.grid_width), None)
-
+        # creating a temporary score variable
+        temp_score = 0
         # loop for checking every row
         for i in range(len(self.tile_matrix)):
             # a boolean variable to check if tile is occupied
@@ -135,15 +137,20 @@ class GameGrid:
                 # if cell is occupied isFull remains true and loop continues
                 if self.is_occupied(i, j):
                     isFull = True
-
-                    # if cell is not occupied isFull is updated to false and inner loop breaks
-
+                    # adding the number of occupied tile to temp_score
+                    temp_score += Tile.get_number(self.tile_matrix[i][j])
+                # if cell is not occupied isFull is updated to false and inner loop breaks
+                # and temp_score is updated to 0 to compute other lines score
                 else:
                     isFull = False
+                    temp_score == 0
                     break
 
             # if isFull is true after checking every cell in a row deleting the row
             # and adding a new empty row to game board
+            # and adding temp_score to score variable
             if isFull == True:
                 self.tile_matrix = np.delete(self.tile_matrix, i, 0)
                 self.tile_matrix = np.append(self.tile_matrix, new_row, 0)
+                self.score += temp_score
+                print(self.score)
